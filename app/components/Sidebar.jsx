5c7677369c01f17a5f67f6d48884f382ef90dfbd
VaradSutar30 <varadsutar30@gmail.com>
2026-01-13 00:33:18 +0530

    project update

--- app/components/Sidebar.jsx
+++ app/components/Sidebar.jsx
@@ -1,51 +1,45 @@
-import Image from "next/image";
+"use client";
+import { IoMdHome } from "react-icons/io";
+import { MdHolidayVillage, MdSecurity } from "react-icons/md";
+import { BiSolidCctv } from "react-icons/bi";
+import { IoMdSettings } from "react-icons/io";
+import { FaHandsHelping } from "react-icons/fa";
+import SidebarButton from "./SidebarButton";
 
 const Sidebar = () => {
   return (
-    <div className="w-56 min-h-screen bg-gray-900 shadow-lg p-4 flex flex-col text-white">
+    <div className="w-56 h-full bg-gray-900 shadow-lg p-4 flex flex-col text-white">
 
       {/* Logo */}
       <div className="flex items-center gap-3 mb-8">
-        <Image
-          src="/images/villa.jfif"
-          alt="logo"
-          width={40}
-          height={40}
-          className="rounded object-cover"
-          priority
-        />
+      <img
+    src="/images/villa.jfif"
+    alt="picture"
+    className="w-40 h-24 object-cover rounded border"
+      />
+
       </div>
 
       {/* Menu */}
-      <nav className="flex flex-col gap-2 flex-1 rounded">
+      <nav className="flex flex-col gap-2 flex-1">
 
-        <button className="px-4 py-2 rounded-lg text-left hover:bg-gray-800 font-medium bg-gray-800">
-          Home
-        </button>
+    <SidebarButton icon={<IoMdHome />} text="Home" link="/" />
+    <SidebarButton icon={<MdHolidayVillage />} text="Rooms" link="/rooms" />
+    <SidebarButton icon={<MdSecurity />} text="Security" link="/notification" />
+    <SidebarButton icon={<BiSolidCctv />} text="CCTV" link="/cctv" />
 
-        <button className="px-4 py-2 rounded-lg text-left hover:bg-gray-800 font-medium">
-          Rooms
-        </button>
+</nav>
 
-        <button className="px-4 py-2 rounded-lg text-left hover:bg-gray-800 font-medium">
-          Security
-        </button>
-
-        <button className="px-4 py-2 rounded-lg text-left hover:bg-gray-800 font-medium">
-          CCTV
-        </button>
-
-      </nav>
 
       {/* Bottom */}
       <div className="flex flex-col gap-2 pt-6 border-t border-gray-700">
 
-        <button className="px-4 py-2 rounded-lg text-left hover:bg-gray-800 font-medium">
-          Settings
+        <button className="flex items-center gap-3 px-4 py-2 rounded-lg text-left hover:bg-gray-800 font-medium">
+          <IoMdSettings /> Settings
         </button>
 
-        <button className="px-4 py-2 rounded-lg text-left hover:bg-gray-800 font-medium">
-          Help
+        <button className="flex items-center gap-3 px-4 py-2 rounded-lg text-left hover:bg-gray-800 font-medium">
+          <FaHandsHelping /> Help
         </button>
 
       </div>
---
+++ app/components/SidebarButton.jsx
@@ -0,0 +1,15 @@
+const SidebarButton = ({ icon, text, link }) => {
+    return (
+      <a
+        href={link}
+        className="flex items-center gap-3 hover:bg-gray-500 px-4 py-2 rounded-lg transition"
+      >
+        {icon}
+        <span>{text}</span>
+      </a>
+  
+      
+    );
+  };
+  
+  export default SidebarButton;
